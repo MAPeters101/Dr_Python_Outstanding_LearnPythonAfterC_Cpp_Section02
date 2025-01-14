ccba6cf2bd66ccf9bca5a55ac2e954808c3a7d21
Mark Peters <mapeters101@earthlink.net>
2025-01-14 16:40:58 -0500

    Work on lesson014 to 5:00.

---
+++ lesson014.py
@@ -0,0 +1,37 @@
+mystr = "ABC XYZ 123"
+i = 0
+while i < len(mystr):
+    print(mystr[i])
+    i += 1
+print()
+
+
+
+# mynum = [6,54,313,66,3,77]
+# for i in mynum:
+#     print(i+1)
+
+# myrange = range(1, 11, 2)
+# for x in myrange:
+#     print(x)
+
+# mydict = {
+#      "id":123,
+#      "name":"Jafri",
+#      "Subject":"Python",
+#      "Contact":283289
+#      }
+#
+# for k,v in mydict.items():
+#     print(k, v)
+# print()
+#
+# for k in mydict.keys():
+#     print(k)
+# print()
+#
+# for v in mydict.values():
+#     print(v)
+# print()
+
+
