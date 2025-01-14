c83460c17c3f7732e796da3828de235162cbfae9
Mark Peters <mapeters101@earthlink.net>
2025-01-14 16:48:20 -0500

    Complete work on lesson014 from 6:10.

--- lesson014.py
+++ lesson014.py
@@ -5,34 +5,18 @@
 #     i += 1
 # print()
 
-mynum = [6,54,313,66,3,77]
+# mynum = [6,54,313,66,3,77]
+# i = 0
+# while i < len(mynum):
+#     print(mynum[i])
+#     i += 1
+# print()
+
+myrange = range(1, 11, 2)
 i = 0
-while i < len(mynum):
-    print(mynum[i])
+while i < len(myrange):
+    print(myrange[i])
     i += 1
 print()
-
-# myrange = range(1, 11, 2)
-# for x in myrange:
-#     print(x)
-
-# mydict = {
-#      "id":123,
-#      "name":"Jafri",
-#      "Subject":"Python",
-#      "Contact":283289
-#      }
-#
-# for k,v in mydict.items():
-#     print(k, v)
-# print()
-#
-# for k in mydict.keys():
-#     print(k)
-# print()
-#
-# for v in mydict.values():
-#     print(v)
-# print()
 
 
