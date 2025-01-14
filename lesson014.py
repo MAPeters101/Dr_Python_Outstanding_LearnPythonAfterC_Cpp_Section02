f1cb177cac11c88bcdd00c22205c1a0ff7211541
Mark Peters <mapeters101@earthlink.net>
2025-01-14 16:43:48 -0500

    Work on lesson014 from 5:00 to 6:10.

--- lesson014.py
+++ lesson014.py
@@ -1,15 +1,16 @@
-mystr = "ABC XYZ 123"
+# mystr = "ABC XYZ 123"
+# i = 0
+# while i < len(mystr):
+#     print(mystr[i])
+#     i += 1
+# print()
+
+mynum = [6,54,313,66,3,77]
 i = 0
-while i < len(mystr):
-    print(mystr[i])
+while i < len(mynum):
+    print(mynum[i])
     i += 1
 print()
-
-
-
-# mynum = [6,54,313,66,3,77]
-# for i in mynum:
-#     print(i+1)
 
 # myrange = range(1, 11, 2)
 # for x in myrange:
