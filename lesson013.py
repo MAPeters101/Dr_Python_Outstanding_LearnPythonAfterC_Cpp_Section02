681cfea3e76ac87581c99b010e8266ac6658b933
Mark Peters <mapeters101@earthlink.net>
2025-01-14 15:53:10 -0500

    Work on lesson013 from 10:35 to 11:36.

--- lesson013.py
+++ lesson013.py
@@ -2,9 +2,11 @@
 # for i in mystr:
 #     print(i)
 
-mynum = [6,54,313,66,3,77]
-for i in mynum:
-    print(i+1)
+# mynum = [6,54,313,66,3,77]
+# for i in mynum:
+#     print(i+1)
 
-
+myrange = range(1, 11, 2)
+for x in myrange:
+    print(x)
 
