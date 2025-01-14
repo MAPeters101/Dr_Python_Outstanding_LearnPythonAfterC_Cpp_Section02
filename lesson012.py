fc4b75aa89fd74c9492694af64c867204ec4ac9a
Mark Peters <mapeters101@earthlink.net>
2025-01-14 15:42:46 -0500

    Work on lesson012.

---
+++ lesson012.py
@@ -0,0 +1,21 @@
+age = 16
+if age >= 18:
+    print("You are okay to participate in the program")
+    print("Cong...")
+    print("Done")
+else:
+    print("Sorry to say!")
+
+marks = int(input("\n\nEnter your marks: "))
+
+if marks >= 90:
+    print("A Grade")
+elif marks >= 80:
+    print("B Grade")
+elif marks >= 70:
+    print("C Grade")
+else:
+    print("You are fail!")
+
+
+
