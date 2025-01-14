1233f4ad93ab57d17caedd0d4b893b4c95ea5ce9
Mark Peters <mapeters101@earthlink.net>
2025-01-14 15:51:18 -0500

    Work on lesson013 from 9:25 to 10:35.

--- lesson013.py
+++ lesson013.py
@@ -1,4 +1,10 @@
-mystr = "ABC XYZ 123"
-for i in mystr:
-    print(i)
+# mystr = "ABC XYZ 123"
+# for i in mystr:
+#     print(i)
+
+mynum = [6,54,313,66,3,77]
+for i in mynum:
+    print(i+1)
+
+
 
