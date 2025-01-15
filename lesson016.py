abfea3f4c0510875c56ed08871937e843af76c5e
Mark Peters <mapeters101@earthlink.net>
2025-01-14 19:49:42 -0500

    Work on lesson016.

---
+++ lesson016.py
@@ -0,0 +1,12 @@
+# Write a program that takes two numbers and prints their sum, difference,
+# product and division.
+
+num1 = float(input("Enter number 1: "))
+num2 = float(input("Enter number 2: "))
+
+print(f"The sum of {num1} and {num2} is {num1 + num2}.")
+print(f"The difference of {num1} and {num2} is {num1 - num2}.")
+print(f"The product of {num1} and {num2} is {num1 * num2}.")
+print(f"The division of {num1} and {num2} is {num1 / num2}.")
+
+
